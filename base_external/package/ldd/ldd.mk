bcbc9301dc40550875ac33cbf406b2cdf8d03704
rkhoury <rkhoury.eng@outlook.com>
2026-09-19 14:39:22 +0300

    Fixes and set latest working commit ID

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-LDD_VERSION = 'main'
+LDD_VERSION = '82a9d9a9ad26226dc2d62d1f12e383b5a8674b0b'
 LDD_SITE = 'git@github.com:RamyKhoury/aeld-assignment-7.git'
 LDD_SITE_METHOD = git
 
